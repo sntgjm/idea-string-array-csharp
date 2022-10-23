b1a9f84feb622a49432341fc13c53baf9921febd
sntgjm <jm2001santiago@gmail.com>
2022-10-23 14:11:24 +0800

    Add project files.

---
+++ Activity 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String[] names = new string[4] { "Secret", "Agency", "Secret", "Agency" };
+            Console.WriteLine(names[3]);
+
+        }
+    }
+}
